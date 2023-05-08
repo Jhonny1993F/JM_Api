0fbfc3f821d8f506428bc2c4143451dca03bafac
laboratorio <laboratorio@UPOAULA10216>
2023-05-08 09:00:33 -0500

    Agregar archivos de proyecto.

---
+++ Data/JmEjercicio2TablasDataContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using JM_Api.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JM_Api.Data;
+
+public partial class JmEjercicio2TablasDataContext : DbContext
+{
+    public JmEjercicio2TablasDataContext()
+    {
+    }
+
+    public JmEjercicio2TablasDataContext(DbContextOptions<JmEjercicio2TablasDataContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Burger> Burgers { get; set; }
+
+    public virtual DbSet<Promo> Promos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=JM_Ejercicio_2_Tablas.Data");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Burger>(entity =>
+        {
+            entity.ToTable("Burger");
+
+            entity.Property(e => e.BurgerId).HasColumnName("BurgerID");
+        });
+
+        modelBuilder.Entity<Promo>(entity =>
+        {
+            entity.ToTable("Promo");
+
+            entity.HasIndex(e => e.BurgerId, "IX_Promo_BurgerId");
+
+            entity.HasOne(d => d.Burger).WithMany(p => p.Promos).HasForeignKey(d => d.BurgerId);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Data/Models/Burger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JM_Api.Data.Models;
+
+public partial class Burger
+{
+    public int BurgerId { get; set; }
+
+    public string? Name { get; set; }
+
+    public bool WithCheese { get; set; }
+
+    public virtual ICollection<Promo> Promos { get; set; } = new List<Promo>();
+}
